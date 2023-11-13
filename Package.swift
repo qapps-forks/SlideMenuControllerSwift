1b985c6d4903e2e4a20c92017e3512a6dd9ba335
QApps Dev Forks <admin@qapps.co>
2023-11-13 14:03:13 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "SlideMenuControllerSwift",
             dependencies: [],
-            path: "SlideMenuControllerSwift"),
+            path: "Source"),
         .testTarget(
             name: "SlideMenuControllerSwiftTests",
             dependencies: ["SlideMenuControllerSwift"],
